5397248c52f48607e081c755947ac425c58fbe2c
ashermanli <ashermanli@gmail.com>
2021-04-12 01:30:24 -0600

    added an input check

--- src/Components/List.js
+++ src/Components/List.js
@@ -5,11 +5,16 @@ const List = ()=>{
 
     const[list,setList] = useState([])
     const[max, setMax] = useState(0)
-    const[input,setInput] = useState('')
+    const[input,setInput] = useState('Add an Entry')
 
     
 
     const handleAdd = ()=>{
+
+        if(input === ''){
+            alert('add some text for the love of god')
+            return
+        }
 
         let listItem = {
             "id":max+1,
@@ -24,6 +29,11 @@ const List = ()=>{
 
         let filteredArray = list.filter(item => item.id !== id)
         setList(filteredArray)
+
+        if(filteredArray.length === 0){
+            setMax(0)
+            setInput('Add an entry')
+        }
     }
 
     const handleInput = (e)=>{
@@ -31,17 +41,23 @@ const List = ()=>{
         setInput(e.target.value)
     }
 
+    const handleInputClick = ()=>{
+        setInput('')
+    }
+
+    
     return(
-        <div className='list'>
+        <div className={list.length >0 ? 'list': 'empty-list'}>
         <ul>
             {list.map(entry => <ListEntry key={entry.id} entry={entry.entry} id={entry.id} handleDelete={() =>handleDelete(entry.id)}></ListEntry>)}
         </ul>
-        <div>
-            <input type='text' value={input} onChange={handleInput}></input>
+        <div className='input-block'>
+            <input type='text' value={input} onClick={handleInputClick} onChange={handleInput}></input>
             <button onClick={handleAdd}>Add</button>
         </div>
         </div>
     )
+
 }
 
 export default List

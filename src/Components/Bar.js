7ae28e50b7dc0718be984a97a0c3b9938a978cf6
ashermanli <ashermanli@gmail.com>
2021-06-18 20:06:53 -0600

    updated bar display options and added styling

--- src/Components/Bar.js
+++ src/Components/Bar.js
@@ -1,34 +1,43 @@
 import React from 'react'
 
-const Bar = ({progWidth, hour, formatTime, time,viewAll}) =>{
+const Bar = ({progWidth, hour, formatTime, time,view}) =>{
 
-    let barArray = []
+  let barArray = []
 
-    const fillBars = () =>{
-        for(let i = 0;i<24;i++){
-            let barItem;
-            if(i < hour){
-                barItem = <div className='bar expired'>{formatTime(i)}</div>
-            }
-            else if (i > hour){
-                barItem = <div className='bar'>{formatTime(i)}</div>
-            }
-            else{
-                barItem = <div className='bar'><div className='progress' style={{width:progWidth}}>{time}</div></div>
-            }
-            barArray = [...barArray, barItem]
-        }
+  const fillBars = (view) =>{
+    
+    for(let i = 0;i<view;i++){
+      //Make time display independent of iteration for different views
+      let display = i
+
+      //If the view chosen is 12 and the time is past 12, show the last 12 hours of the day
+      if(view === 12){
+        if(hour >= 12)display+=12
+      }
+
+      let barItem
+      if(display < hour){
+        barItem = <div className='bar expired'>{formatTime(display)}</div>
+      }
+      else if (display > hour){
+        barItem = <div className='bar'>{formatTime(display)}</div>
+      }
+      else{
+        barItem = <div className='bar'><div className='progress' style={{width:progWidth}}>{time}</div></div>
+      }
+      barArray = [...barArray, barItem]
     }
+  }
     
 
-    fillBars()
+  fillBars(view)
 
-    // console.log(barArray)
+  // console.log(barArray)
   return (
-      <>
-          {viewAll ? barArray.map(item => item):
-          <div className='bar'><div className='progress' style={{width:progWidth}}></div></div>}
-      </>
+    <>
+      {view !== 1 ? barArray.map(item => item):
+        <div className='bar'><div className='progress' style={{width:progWidth}}></div></div>}
+    </>
     
   )
 }

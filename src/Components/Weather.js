23fcef246c85748038976bd9c96e8c99556b588b
ashermanli <ashermanli@gmail.com>
2021-06-23 09:33:06 -0600

    updated weather display and made it responsive

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -62,6 +62,7 @@ const Weather = ({hours, formatTime}) =>{
         })
         .then(data => {
           setWeatherData(data)
+          // console.log(data)
                 
           //for debugging
           // const time = new Date(data.current.dt *1000)
@@ -88,11 +89,16 @@ const Weather = ({hours, formatTime}) =>{
     //Fill an object with the weather information we desire
     const fillInfo = ()=>{
       const fill = {
+        'forecast' :
+        [
+          weatherData.current.weather[0].description,
+          weatherData.current.weather[0].icon
+        ],
         'temp': weatherData.current.temp,
         'sunrise': weatherData.current.sunrise,
         'sunset': weatherData.current.sunset,
         'wind-Speed': weatherData.current.wind_speed,
-        'windDeg': weatherData.current.wind_deg
+        'windDeg': weatherData.current.wind_deg, 
       }
     
       //create iterable key value pair array
@@ -101,11 +107,23 @@ const Weather = ({hours, formatTime}) =>{
       let infoArray = []
     
       for(const [key,value] of entries){
-        const text = (key === 'temp') ? `${key.toUpperCase()} : ${value}°F`: 
-          (key === 'sunrise' || key === 'sunset')? `${key.toUpperCase()} ${formatTime(new Date(value *1000).getHours())} : ${formatTime(new Date(value *1000).getMinutes())}`:
-            (key === 'windDeg')? `WIND-DIR : ${degToCompass(value)}`:
-              `${key.toUpperCase()} : ${value}`
-        infoArray = [...infoArray,text]
+        // Convert the key to all uppercase ot use for display
+        let attribute = key.toUpperCase()
+
+        //Grab the icon for the current weather
+        let iconCode
+        if(key === 'forecast'){
+          iconCode = `http://openweathermap.org/img/w/${value[1]}.png`
+        }
+
+        const content = 
+        (key === 'forecast') ? <div className='flex flex-row items-center'><img src={iconCode} alt='Weather icon' className='mr-2'/><p>{value[0].toUpperCase()}</p></div> :
+          (key === 'temp') ? `${attribute} : ${value}°F`: 
+            (key === 'sunrise' || key === 'sunset')? `${attribute} ${formatTime(new Date(value *1000).getHours())} : ${formatTime(new Date(value *1000).getMinutes())}`:
+              (key === 'windDeg')? `WIND-DIR : ${degToCompass(value)}`:
+                `${attribute} : ${typeof(value) === 'string' ? value.toUpperCase() : value}`
+
+        infoArray = [...infoArray,content]
         // console.log(infoArray)
         setInfo(infoArray)
     
@@ -127,13 +145,14 @@ const Weather = ({hours, formatTime}) =>{
 
     
   return(
-    <div className='loading self-start'>
+    <>
       {info.length === 0 ? 'Awaiting Data': 
-        <ul id='weather' className='flex flex-row space-x-2'>
-          {info.map(entry => <li key={entry}className='border-2 border-green-500'> {entry} </li>)}
+        <ul id='weather' className='md:h-6 w-screen md:w-auto flex flex-col flex-shrink-0 items-center md:items-stretch md:flex-row md:space-x-2'>
+          {info.map(entry => <li key={entry}className='p-2 flex flex-row items-center md:border-r border-green-500'> {entry} </li>)}
         </ul>
       }
-    </div>
+    </>
+    
   )
     
 }
